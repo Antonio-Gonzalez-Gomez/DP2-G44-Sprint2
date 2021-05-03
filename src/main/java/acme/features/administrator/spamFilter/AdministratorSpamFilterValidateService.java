5d18a2b2724e3ea522a76f05e93741157df6b58c
Antonio González Gómez <antoniogg696@gmail.com>
2021-05-04 00:46:49 +0200

    Reworking spam filter (again)

--- src/main/java/acme/features/administrator/spamFilter/AdministratorSpamFilterValidateService.java
+++ src/main/java/acme/features/administrator/spamFilter/AdministratorSpamFilterValidateService.java
@@ -1,5 +1,6 @@
 package acme.features.administrator.spamFilter;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,7 +15,17 @@ public class AdministratorSpamFilterValidateService {
 	@Autowired
 	protected AdministratorSpamFilterRepository repository;
 	
-	public boolean validate(final String input) {
+	private boolean compoundWordEquals(final String[] words, final List<String> text, int i, final int j) {
+		if (i == words.length)
+			return true;
+		if (text.size() <= i + j)
+			return false;
+		else {
+			return words[i].equals(text.get(i + j)) && this.compoundWordEquals(words, text, ++i, j);
+		}
+	}
+	
+	public boolean validate2(final String input) {
 		String text = input.replace(" ", "").toLowerCase();
         final int total = text.length();
         if (total == 0)
@@ -26,7 +37,56 @@ public class AdministratorSpamFilterValidateService {
         for (final String word : words)
             text = text.replace(word,"");
         final int noSpam = text.length();
-        
+        System.out.println("THRESHOLD: " + filter.getThreshold());
+        System.out.println("CARACTERES TOTALES: " + total);
+        System.out.println("CARACTERES NO SPAM: " + noSpam);
+        return 100*(total - noSpam)/total >= filter.getThreshold();
+	}
+	
+	public boolean validate(final String input) {
+		List<String> text;
+		text = Arrays.asList(input.toLowerCase().trim().split("\\s+"));
+		System.out.println("INPUT: " + text);
+		int i = 0, j = 0, k;
+		final int total = text.size();
+		
+		final Filter filter = this.repository.findFilters().get(0);
+		final List<String[]> words = this.repository.findSpamWords().stream()
+			.map(x -> x.getWord().toLowerCase().trim().split("\\s+"))
+			.collect(Collectors.toList());
+		
+		while (i < words.size()) {
+			final String[] word = words.get(0);
+			System.out.println("PALABRA SPAM: " + word[0]);
+			j = 0;
+			
+			if (word.length == 1) {
+				while (j<text.size())
+					if (text.get(j).equals(word[0]))
+						text.remove(j);
+					else
+						j++;
+			}
+			
+			else
+			{
+				while (j<text.size()) {
+					if (this.compoundWordEquals(word, text, i, j)) {
+						for (k=0 ; k<word.length ; k++)
+							text.remove(k + j);
+					}
+					else
+						j++;
+				}
+			}
+			
+			i++;
+		}
+		
+		final int noSpam = text.size();
+        System.out.println("THRESHOLD: " + filter.getThreshold());
+        System.out.println("PALABRAS TOTALES: " + total);
+        System.out.println("PALABRAS NO SPAM: " + noSpam);
         return 100*(total - noSpam)/total >= filter.getThreshold();
 	}
 }
