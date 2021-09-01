18a343645815e2007f7651332ebce8228551550d
Antonio González Gómez <antoniogg696@gmail.com>
2021-09-01 13:13:46 +0200

    Fixed requirements in Shout, Task and Dashboard

--- src/main/java/acme/entities/shouts/Shout.java
+++ src/main/java/acme/entities/shouts/Shout.java
@@ -8,6 +8,7 @@ import javax.persistence.TemporalType;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.URL;
 
@@ -32,9 +33,11 @@ public class Shout extends DomainEntity {
 	protected Date				moment;
 
 	@NotBlank
+	@Size(min = 5, max = 25, message = "Min 5 characters, max 25")
 	protected String			author;
 
 	@NotBlank
+	@Size(max = 100, message = "Max 100 characters")
 	protected String			text;
 	
 	@URL
